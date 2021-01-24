db529a232635d388c52cbc4c54c5a4f6b9ba2760
worldluoji <qq1132416@outlook.com>
2021-01-24 14:08:00 +0800

    @pipline demo@luoji@20210124

--- goroutine/goroutine_test.go
+++ goroutine/goroutine_test.go
@@ -51,3 +51,66 @@ func TestSelectChannelCase(t *testing.T) {
 		// 	t.Log("什么也没找到")
 	}
 }
+
+func echo(nums []int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func sq(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func odd(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			if n%2 == 1 {
+				out <- n
+			}
+		}
+		close(out)
+	}()
+	return out
+}
+
+// pipeline实际就是用channel将结果连接起来
+func TestPipeline1(t *testing.T) {
+	nums := []int{1, 2, 3, 4, 5, 6}
+	// in fact , it is a pipeline
+	for r := range odd(sq(echo(nums))) {
+		fmt.Println(r)
+	}
+}
+
+type EchoFunc func([]int) <-chan int
+type PipelineFunc func(<-chan int) <-chan int
+
+// 利用函数式编程对Pipeline进行封装
+func Pipeline(nums []int, echoFunc EchoFunc, pipelineFuncs ...PipelineFunc) <-chan int {
+	ch := echoFunc(nums)
+	for _, f := range pipelineFuncs {
+		ch = f(ch)
+	}
+	return ch
+}
+
+func TestPipeline2(t *testing.T) {
+	out := Pipeline([]int{1, 2, 3, 4, 5, 6}, echo, sq, odd)
+	for r := range out {
+		fmt.Println(r)
+	}
+}
