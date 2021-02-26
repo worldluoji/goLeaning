6e274e1865050ade4492eac2d6e4f7d37a1b123f
worldluoji <qq1132416@outlook.com>
2021-02-26 17:21:41 +0800

    @slice demo@luoji@20210226

--- slicetest/slice_test.go
+++ slicetest/slice_test.go
@@ -59,3 +59,13 @@ func TestArrayToSliceEqual(t *testing.T) {
 	s2 := []int{1, 2, 3}
 	fmt.Println(reflect.DeepEqual(s1, s2))
 }
+
+func TestArrayToSliceAppend(t *testing.T) {
+	s1 := []int{1}
+	s2 := append(s1, 4)
+	s3 := append(s1, 5)
+	s2[0] = 8
+	fmt.Println(s1, len(s1), cap(s1))
+	fmt.Println(s2, len(s2), cap(s2))
+	fmt.Println(s3, len(s3), cap(s3))
+}
