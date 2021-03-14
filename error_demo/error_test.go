df30fac45c6141c4da74300b8f8859903805a2d4
worldluoji <qq1132416@outlook.com>
2021-03-14 15:00:51 +0800

    @recover@luoji@20210314

--- error_demo/error_test.go
+++ error_demo/error_test.go
@@ -2,6 +2,7 @@ package error_demo
 
 import (
 	"errors"
+	"fmt"
 	"testing"
 )
 
@@ -22,6 +23,10 @@ func TestErrorCase1(t *testing.T) {
 	}
 }
 
+/*
+* 服务可能会发生未知的panic，这时最好的方式就是使用recover兜底，防止因为报错直接退出
+* 另外需要注意的是，panic不要乱用，可以明确处理返回错误的，就通过返回值判断。
+ */
 func TestRecoverCase1(t *testing.T) {
 	defer func() {
 		if err := recover(); err != nil {
@@ -33,6 +38,27 @@ func TestRecoverCase1(t *testing.T) {
 	} else {
 		panic(err)
 	}
+}
+
+func Go(f func()) {
+	go func() {
+		defer func() {
+			if err := recover(); err != nil {
+				fmt.Println(err)
+			}
+		}()
+		// 如果发生panic，仍然会执行defer中的东东
+		f()
+	}()
+}
+
+func TestRecoverCase2(t *testing.T) {
+	// 这样进行一个封装，当函数中业务处理发生严重错误时，也不会把主线程搞挂，因为这样发生了panic，就会走到recover
+	Go(func() {
+		t.Log("Hello")
+		// 模拟业务发生panic
+		panic("业务处理发生严重错误")
+	})
 }
 
 func TestPanic(t *testing.T) {
