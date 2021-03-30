b622ac6b9b4e85a161500055feaaf9c47ad0262e
worldluoji <qq1132416@outlook.com>
2021-03-30 17:00:30 +0800

    @empty struct@luoji@20210330

--- struct_demo/struct_test.go
+++ struct_demo/struct_test.go
@@ -1,6 +1,9 @@
 package struct_demo
 
-import "testing"
+import (
+	"testing"
+	"unsafe"
+)
 
 type User struct {
 	Name    string
@@ -32,4 +35,23 @@ func TestCreateAndInitStructCase3(t *testing.T) {
 	user.Age = 29
 	user.address = "Chendu"
 	t.Log(user.Name, user.Age, user.address)
+}
+
+type S struct {
+	a uint16
+	b uint32
+}
+
+func TestEmptyStruct(t *testing.T) {
+	var s string
+	var c complex128
+	var a [3]uint32
+	var st S
+	t.Log(unsafe.Sizeof(s))  // 16
+	t.Log(unsafe.Sizeof(c))  // 16
+	t.Log(unsafe.Sizeof(a))  // 12 = 3 * （32 / 8）
+	t.Log(unsafe.Sizeof(st)) // 8 = 16 / 8 + 16 / 8 + 32 / 8 和C语言字节对齐一样
+
+	var st2 struct{}
+	t.Log(unsafe.Sizeof(st2)) // 0 因为空结构体不占用任何空间，因此就不存在内存对齐的问题。因此多个空结构体类型组合成的结构体也不占用内存。
 }
