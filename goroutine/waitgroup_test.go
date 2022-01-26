593756d02bc936ff67c6dad1db2aa65bd34a9046
worldluoji <qq1132416@outlook.com>
2022-01-26 11:15:09 +0800

    @channel@luoji@20220126

--- goroutine/waitgroup_test.go
+++ goroutine/waitgroup_test.go
@@ -59,3 +59,43 @@ func TestMultyReceiver(t *testing.T) {
 	wg.Wait()
 	t.Log("程序所有协程全部执行完毕")
 }
+
+type counter struct {
+	c chan int
+	i int
+}
+
+func NewCounter() *counter {
+	cter := &counter{
+		c: make(chan int),
+	}
+	go func() {
+		for {
+			cter.i++
+			cter.c <- cter.i
+		}
+	}()
+	return cter
+}
+
+func (cter *counter) Increase() int {
+	return <-cter.c
+}
+
+/*
+* 利用无缓冲信道，发一定在收之前，否则收阻塞的特性，实现一个counter
+ */
+func TestWaitGroupCase2(t *testing.T) {
+	cter := NewCounter()
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(i int) {
+			v := cter.Increase()
+			fmt.Printf("goroutine-%d: current counter value is %d\n", i, v)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+	fmt.Println(cter.i)
+}
