ddd64d263dafc592daeb1b123e39f40c94f2eb88
worldluoji <qq1132416@outlook.com>
2022-01-28 14:00:06 +0800

    @combine@luoji@20220128

--- struct_demo/struct_test.go
+++ struct_demo/struct_test.go
@@ -56,3 +56,47 @@ func TestEmptyStruct(t *testing.T) {
 	var st2 struct{}
 	t.Log(unsafe.Sizeof(st2)) // 0 因为空结构体不占用任何空间，因此就不存在内存对齐的问题。因此多个空结构体类型组合成的结构体也不占用内存。
 }
+
+type MyInt int
+
+func (n *MyInt) Add(m int) {
+	*n = *n + MyInt(m)
+}
+
+type t struct {
+	a int
+	b int
+}
+
+type S struct {
+	*MyInt
+	t
+	io.Reader
+	s string
+	n int
+}
+
+/*
+* 嵌入字段类型的底层类型不能为指针类型。而且，嵌入字段的名字在结构体定义也必须是唯一的，
+* 这也意味这如果两个类型的名字相同，它们无法同时作为嵌入字段放到同一个结构体定义中。
+* 不过，这些约束你了解一下就可以了，一旦违反，Go 编译器会提示你的。
+ */
+func TestStructCombine(t *testing.T) {
+	m := MyInt(17)
+	r := strings.NewReader("hello, go")
+	s := S{
+		MyInt: &m,
+		t: t{
+			a: 1,
+			b: 2,
+		},
+		Reader: r,
+		s:      "demo",
+	}
+
+	var sl = make([]byte, len("hello, go"))
+	s.Reader.Read(sl)       // 注意这里是s.Reader而不是s.io.Reader，Go语语法的规定
+	fmt.Println(string(sl)) // hello, go
+	s.MyInt.Add(5)
+	fmt.Println(*(s.MyInt)) // 22
+}
