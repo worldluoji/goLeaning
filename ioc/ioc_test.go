f9fcf4f469154563f2fdd5643b8c7e81cff3eeb6
worldluoji <qq1132416@outlook.com>
2022-03-22 16:48:47 +0800

    @ioc@luoji@20220322

--- ioc/ioc_test.go
+++ ioc/ioc_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-/*
+/* 下面的例子，实现了一个带撤销操作的set
 ** 控制反转，不是由控制逻辑 Undo  来依赖业务逻辑 IntSet，而是由业务逻辑 IntSet 依赖 Undo 。
 ** 这里依赖的是其实是一个协议，这个协议是一个没有参数的函数数组。可以看到，这样一来，我们 Undo 的代码就可以复用了。
 ** 它的主要思想是把控制逻辑与业务逻辑分开，不要在业务逻辑里写控制逻辑，因为这样会让控制逻辑依赖于业务逻辑，
@@ -75,6 +75,8 @@ func TestIoc(t *testing.T) {
 	set = set.NewInSet()
 	set.Add(1)
 	set.Add(2)
-	set.undo.Undo()
+	if err := set.undo.Undo(); err != nil {
+		t.Log(err)
+	}
 	t.Log(set.Contains(1), set.Contains(2))
 }
