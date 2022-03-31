80a3c5ddb1edd47e0bab23911e4c0d28a19db3d6
worldluoji <qq1132416@outlook.com>
2022-03-31 14:05:58 +0800

    @rand@luoji@20220331

--- other_demo/rand_test.go
+++ other_demo/rand_test.go
@@ -1,15 +1,20 @@
 package other_demo
 
 import (
+	"fmt"
 	"math/rand"
 	"testing"
 	"time"
-	"fmt"
 )
 
 func TestRandInt(t *testing.T) {
-	rand.Seed(time.Now().UnixNano()) // 设置随机种子，否则每次运行结果会一样
-	num := rand.Intn(100) // [0,100)
-	key := fmt.Sprintf("stu%d", num) 
-	t.Log(key)
+	var se int64 = time.Now().UnixNano()
+	t.Log(se)
+	rand.Seed(se) // 设置随机种子，否则每次运行结果会一样, 只要se变化了，随机序列就会变
+	for i := 0; i < 3; i++ {
+		num := rand.Intn(100) // [0,100)
+		key := fmt.Sprintf("stu%d", num)
+		t.Log(key)
+	}
+
 }
