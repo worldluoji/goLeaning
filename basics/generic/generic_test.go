6ada3527569562dff35f4cd09b0b3e832ec30f22
worldluoji <qq1132416@outlook.com>
2022-11-22 17:15:57 +0800

    feat: add generic demo

---
+++ basics/generic/generic_test.go
@@ -0,0 +1,20 @@
+package generic
+
+import (
+	"testing"
+
+	"golang.org/x/exp/constraints"
+)
+
+func Add[T constraints.Integer](a, b T) T {
+	return a + b
+}
+
+func TestGeneric(t *testing.T) {
+	var m, n int = 5, 6
+	println(Add(m, n)) // Add[int](m, n)
+	var i, j int64 = 15, 16
+	println(Add(i, j)) // Add[int64](i, j)
+	var c, d byte = 0x11, 0x12
+	println(Add(c, d)) // Add[byte](c, d)
+}
